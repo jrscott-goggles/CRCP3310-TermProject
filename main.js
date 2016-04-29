5824ff9a4104afd9996dd940adb1a30e98acae5f
jrscott-goggles <justin@scottfamily.com>
2016-04-29 10:47:39 -0500

    Fixed country coloring and removed junk
    Countries were previously being colored based on values of other
    countries.  Created new array sorted_names to allow matching of country
    borders and data properly.
    Removed old testing code.

--- main.js
+++ main.js
@@ -31,7 +31,6 @@ function ready(error, world, names) {
       land = topojson.feature(world, world.objects.land),
       countries = topojson.feature(world, world.objects.countries).features,
       borders = topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }),
-      i = -1,
       n = countries.length;
 	
 	var dragBehaviour = d3.behavior.drag()
@@ -55,46 +54,47 @@ function ready(error, world, names) {
 		projection.rotate(rotation);
 		drawGlobe();
 	}) 
-  /*var colors = [];
-  var j;
-  for (j = 0; j < n; j++) {
-	  colors.push();
-  }*/
-  var color = d3.scale.category20();
+	
 
-  countries = countries.filter(function(d) {
-    return names.some(function(n) {
-      if (d.id == n.id) return d.name = n.name;
-    });
-  }).sort(function(a, b) {
-    return a.name.localeCompare(b.name);
-  });  
+	var i,
+		sorted_names = [];
+	for (i = 0; i < n; i++) {
+		current_id = countries[i].id;
+		var j, match_found = false;
+		for (j = 0; j < names.length; j++) {
+			if (names[j].id == current_id) {
+				sorted_names.push(names[j]);
+				match_found = true;
+			}
+		}
+		if (!match_found) {
+			sorted_names.push({id: current_id, name: "", yr_1990: "",yr_2000: "", yr_2006: "", yr_2007: "", yr_2008: "", yr_2009: "", yr_2010: "", yr_2011: ""});
+		}
+	}
+  
   
   drawGlobe();
   d3.select("body").select('canvas').call(dragBehaviour);
   
+  
   function drawGlobe() {
 	c.clearRect(0, 0, width, height);
-	//c.fillStyle = "blue", c.beginPath(), path(land), c.fill(); //color of countries
-	//c.fillStyle = color(j%20), c.beginPath(), path(countries[j]), c.fill();
 	var j;
 	for (j = 0; j < n; j++) {
-		c.fillStyle = color(j%20), c.beginPath(), path(countries[j]), c.fill();
 		var countryColor = d3.rgb(192, 192, 192);
-		if (names[j].yr_1990 > 50000) {
+		if (sorted_names[j].yr_1990 > 50000) {
 		  countryColor = d3.rgb(204, 51, 0);
-		} else if (names[j].yr_1990 < 50000 && names[j].yr_1990 > 10000) {
+		} else if (sorted_names[j].yr_1990 < 50000 && sorted_names[j].yr_1990 > 10000) {
 		  countryColor = d3.rgb(255, 102, 0);
-		} else if (names[j].yr_1990 < 10000 && names[j].yr_1990 > 2000) {
+		} else if (sorted_names[j].yr_1990 < 10000 && sorted_names[j].yr_1990 > 2000) {
 		  countryColor = d3.rgb(255, 153, 0);
-		} else if (names[j].yr_1990 < 2000 && names[j].yr_1990 > 400) {
+		} else if (sorted_names[j].yr_1990 < 2000 && sorted_names[j].yr_1990 > 400) {
 			countryColor = d3.rgb(255, 204, 0);
 		} else {
 			countryColor = d3.rgb(255, 255, 153);
 		}
 		c.fillStyle = countryColor, c.beginPath(), path(countries[j]), c.fill();
 	}
-	//c.fillStyle = "black", c.beginPath(), path(countries[i]), c.fill();  //selected country
 	c.strokeStyle = "#aaa", c.lineWidth = .5, c.beginPath(), path(borders), c.stroke();
 	c.strokeStyle = "#000", c.lineWidth = 2, c.beginPath(), path(globe), c.stroke();
 
@@ -102,30 +102,3 @@ function ready(error, world, names) {
 }
 
 d3.select(self.frameElement).style("height", height + "px");
-
-var dataset;
-
-d3.csv("data/emissions.csv", function(error, data) {
-  if (error) {
-    console.log(error);
-  } else {
-    //console.log(data);
-    dataset = data;
-    d3.select("body").selectAll("p")
-      .data(dataset)
-      .enter()
-      .append("p")
-      .style("color", function(d) {
-        if (d.yr_1990 > 100000) {
-          return "red";
-        } else if (d.yr_1990 < 100000 && d.yr_1990 > 10000) {
-          return "black";
-        } else {
-          return "green";
-        }
-      })
-      .text(function(d) { 
-        return d.country_name + " had " + d.yr_1990 + " kt of emissions in 1990.";
-      })
-  }
-});
